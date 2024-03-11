53cddc43bf435a1b0065de037d344371967a17d7
Diognet <morgan.up.dn@gmail.com>
2024-03-11 14:50:36 +0300

    Generic Props

--- src/components/generics/List.tsx
+++ src/components/generics/List.tsx
@@ -2,17 +2,17 @@ type ListProps<T> = {
   items: T[];
   onClick: (value: T) => void;
 };
-export const List = <T extends { id: number }>({
+export const List = <T extends { id: number; last: string; first: string }>({
   items,
   onClick,
 }: ListProps<T>) => {
   return (
     <div>
       <h2>List of items</h2>
-      {items.map((item, index) => {
+      {items.map((item) => {
         return (
           <div key={item.id} onClick={() => onClick(item)}>
-            {item}
+            {item.id}
           </div>
         );
       })}
